7b1b0627298fcd8b723706cf817618099963faf5
Allan77230 <predaboss@gmail.com>
2017-11-27 14:08:35 +0100

    VIEW: Positionnement des et reglage du grid pane

--- src/userInterface/Viewer.java
+++ src/userInterface/Viewer.java
@@ -69,9 +69,9 @@ public class Viewer implements ViewerService, RequireReadService{
 	  GridPane gridpane_malette = new GridPane();
 	  
 	  //Gestion des contraintes de colonne style padding ..
-	  gridpane_malette.setPadding(new Insets(0,0,0,0));
+	  gridpane_malette.setPadding(new Insets(15,0,0,0));
 	  gridpane_malette.setHgap(15);
-	  gridpane_malette.setVgap(15);
+	  gridpane_malette.setVgap(10);
 	  
 	  ColumnConstraints column1 = new ColumnConstraints(-10 + (3*HardCodedParameters.defaultWidth/4)/3, -10 + (3*HardCodedParameters.defaultWidth/4)/3, -10 + (3*HardCodedParameters.defaultWidth/4)/3);
 	  ColumnConstraints column2 = new ColumnConstraints(-10 + (3*HardCodedParameters.defaultWidth/4)/3, -10 + (3*HardCodedParameters.defaultWidth/4)/3, -10 + (3*HardCodedParameters.defaultWidth/4)/3);
@@ -83,8 +83,8 @@ public class Viewer implements ViewerService, RequireReadService{
 	  gridpane_malette.getColumnConstraints().addAll(column1, column2, column3);	  
 	 
 	  //Background de la malette(du gridpane)
-	  BackgroundImage fond_malette= new BackgroundImage(new Image("/images/bordure.png",3*HardCodedParameters.defaultWidth/4,3*HardCodedParameters.defaultHeight/4,false,true),
-		        BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+	  BackgroundImage fond_malette= new BackgroundImage(new Image("/images/bordure.png",3*HardCodedParameters.defaultWidth/4,30+3*HardCodedParameters.defaultHeight/4,false,true),
+		        BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
 		          BackgroundSize.DEFAULT);
 	  gridpane_malette.setBackground(new Background(fond_malette));
 	  
@@ -94,8 +94,8 @@ public class Viewer implements ViewerService, RequireReadService{
 	  //Zone en haut à gauche
 	  Image image_block6 = new Image("/images/block.png");
 	  ImageView zone1 = new ImageView();
-	  zone1.setFitWidth(((3*HardCodedParameters.defaultWidth/4)/3)-15);
-	  zone1.setFitHeight(((3*HardCodedParameters.defaultHeight/4)/2)-30);
+	  zone1.setFitWidth(((3*HardCodedParameters.defaultWidth/4)/3)-20);
+	  zone1.setFitHeight(((3*HardCodedParameters.defaultHeight/4)/2)-8);
 	  zone1.setImage(image_block6);
 	  gridpane_malette.add(zone1,0,0);
 	  GridPane.setMargin(zone1, new Insets(0, 0, 0, 20));
@@ -103,35 +103,35 @@ public class Viewer implements ViewerService, RequireReadService{
 	  //Zone en bas à gauche
 	  Image image_block5 = new Image("/images/block.png");
 	  ImageView zone2 = new ImageView();
-	  zone2.setFitWidth(((3*HardCodedParameters.defaultWidth/4)/3)-15);
-	  zone2.setFitHeight(((3*HardCodedParameters.defaultHeight/4)/2)-30);
+	  zone2.setFitWidth(((3*HardCodedParameters.defaultWidth/4)/3)-20);
+	  zone2.setFitHeight(((3*HardCodedParameters.defaultHeight/4)/2)-15);
 	  zone2.setImage(image_block5);
 	  gridpane_malette.add(zone2,0,1);
-	  GridPane.setMargin(zone2, new Insets(0, 0, 0, 20));
+	  GridPane.setMargin(zone2, new Insets(8, 0, 0, 20));
 	  
 	  //Zone en haut au milieu
 	  Image image_block4 = new Image("/images/block.png");
 	  ImageView zone3 = new ImageView();
 	  zone3.setFitWidth(((3*HardCodedParameters.defaultWidth/4)/3)-35);
-	  zone3.setFitHeight(((3*HardCodedParameters.defaultHeight/4)/2)-30);
+	  zone3.setFitHeight(((3*HardCodedParameters.defaultHeight/4)/2)-8);
 	  zone3.setImage(image_block4);
 	  gridpane_malette.add(zone3,1,0);
-	  GridPane.setMargin(zone3, new Insets(0, 0, 0, 15));
+	  GridPane.setMargin(zone3, new Insets(0, 0, 0, 13));
 	  
 	  //Zone en bas au milieu
 	  Image image_block = new Image("/images/block.png");
 	  ImageView zone4 = new ImageView();
 	  zone4.setFitWidth(((3*HardCodedParameters.defaultWidth/4)/3)-35);
-	  zone4.setFitHeight(((3*HardCodedParameters.defaultHeight/4)/2)-30);
+	  zone4.setFitHeight(((3*HardCodedParameters.defaultHeight/4)/2)-15);
 	  zone4.setImage(image_block);
 	  gridpane_malette.add(zone4,1,1);
-	  GridPane.setMargin(zone4, new Insets(0, 0, 0, 15));
+	  GridPane.setMargin(zone4, new Insets(8, 0, 0, 13));
 
 	  //Zone en haut à droite
 	  Image image_block3 = new Image("/images/block.png");
 	  ImageView zone5 = new ImageView();
 	  zone5.setFitWidth(((3*HardCodedParameters.defaultWidth/4)/3)-20);
-	  zone5.setFitHeight(((3*HardCodedParameters.defaultHeight/4)/2)-30);
+	  zone5.setFitHeight(((3*HardCodedParameters.defaultHeight/4)/2)-8);
 	  zone5.setImage(image_block3);
 	  gridpane_malette.add(zone5,2,0);
 	  GridPane.setMargin(zone5, new Insets(0, 0, 0, -10));
@@ -140,10 +140,10 @@ public class Viewer implements ViewerService, RequireReadService{
 	  Image image_block2 = new Image("/images/block.png");
 	  ImageView zone6 = new ImageView();
 	  zone6.setFitWidth(((3*HardCodedParameters.defaultWidth/4)/3)-20);
-	  zone6.setFitHeight(((3*HardCodedParameters.defaultHeight/4)/2)-30);
+	  zone6.setFitHeight(((3*HardCodedParameters.defaultHeight/4)/2)-15);
 	  zone6.setImage(image_block2);
 	  gridpane_malette.add(zone6,2,1);
-	  GridPane.setMargin(zone6, new Insets(0, 0, 0, -10));
+	  GridPane.setMargin(zone6, new Insets(8, 0, 0, -10));
 	  
 	  
 	  
